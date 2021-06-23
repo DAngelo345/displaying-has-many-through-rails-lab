87bb6eff3e7964b0db64f339a0844f83447f411f
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-06-23 17:14:14 -0400

    commit tree 7d28299629db298227bb635c5ae1554b3edffe8d

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,15 +1,15 @@
 class PatientsController < ApplicationController
     def index
         @patients = Patient.all 
+      
     end
 
     def show
         @patient = Patient.find(params[:id])
-
-        
-        binding.pry
-        
     end
+
+
+    private
 
     
 
